fd177934954a094da15ea37e547779192d7cf0c1
junhaa <2171326@hansung.ac.kr>
2024-05-17 23:45:42 +0900

    :tada: setup

---
+++ src/main/java/com/shipdoc/ShipdocApplication.java
@@ -0,0 +1,13 @@
+package com.shipdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipdocApplication.class, args);
+	}
+
+}
