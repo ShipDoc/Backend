a416b4dddf36404046c406e9ddbf296016e5a751
junhaa <2171326@hansung.ac.kr>
2024-05-18 18:01:21 +0900

    :sparkles: FEAT. 요청 DTO 필드 이름 변경

--- src/main/java/com/shipdoc/domain/Member/web/dto/MemberRequestDto.java
+++ src/main/java/com/shipdoc/domain/Member/web/dto/MemberRequestDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 public class MemberRequestDto {
 
@@ -15,12 +16,13 @@ public class MemberRequestDto {
 	 */
 	@Getter
 	@Setter
+	@ToString
 	@PasswordMatch
 	public static class SignupRequestDto{
-		@Email(message = "올바른 이메일 형식으로 작성해주세요.")
 		@NotBlank(message = "이메일을 입력해주세요.")
-		private String login_id;
-		@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$\n", message = "영문, 숫자를 포함한 8~20자리 이내로 입력해주세요.")
+		@Email(message = "올바른 이메일 형식으로 작성해주세요.")
+		private String loginId;
+		@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d\\S]{8,20}$", message = "영문, 숫자를 포함한 8~20자리 이내로 입력해주세요.")
 		private String password;
 		private String passwordCheck;
 	}
