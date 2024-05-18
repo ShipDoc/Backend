a2ed7f265cc2a24e625f97acdd70532266002450
junhaa <2171326@hansung.ac.kr>
2024-05-18 15:13:50 +0900

    :sparkles: FEAT. 유저 권한 엔티티 매핑

---
+++ src/main/java/com/shipdoc/domain/Member/entity/Role.java
@@ -0,0 +1,46 @@
+package com.shipdoc.domain.Member.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shipdoc.domain.Member.entity.mapping.MemberRole;
+import com.shipdoc.domain.Member.enums.Authority;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private Authority authority;
+
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<MemberRole> memberRoleList = new ArrayList<>();
+
+	// 연관 관계 편의 메서드
+	public void addMemberRole(MemberRole memberRole){
+		memberRoleList.add(memberRole);
+		memberRole.setRole(this);
+	}
+}
---
+++ src/main/java/com/shipdoc/domain/Member/enums/Authority.java
@@ -0,0 +1,8 @@
+package com.shipdoc.domain.Member.enums;
+
+/**
+ * 회원 접근 권한 Enum
+ */
+public enum Authority {
+	ROLE_ADMIN, ROLE_GUEST, ROLE_USER
+}
