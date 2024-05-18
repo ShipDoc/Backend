923c220a91872927d4b2cfc68dcaf2deab1da5a8
junhaa <2171326@hansung.ac.kr>
2024-05-18 18:00:14 +0900

    :art: CHORE. 권한 Key ID 추가

--- src/main/java/com/shipdoc/domain/Member/enums/Authority.java
+++ src/main/java/com/shipdoc/domain/Member/enums/Authority.java
@@ -4,5 +4,15 @@ package com.shipdoc.domain.Member.enums;
  * 회원 접근 권한 Enum
  */
 public enum Authority {
-	ROLE_ADMIN, ROLE_GUEST, ROLE_USER
+	ROLE_ADMIN(1L), ROLE_GUEST(2L), ROLE_USER(3L);
+
+	private final Long id;
+
+	Authority(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
