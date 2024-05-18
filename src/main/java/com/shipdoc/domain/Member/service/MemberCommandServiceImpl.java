2a770c90127070a1b5e1012e85c682ecf20431c8
junhaa <2171326@hansung.ac.kr>
2024-05-18 18:02:22 +0900

    :sparkles: FEAT. 사용자 권한 부여 기능 추가

--- src/main/java/com/shipdoc/domain/Member/service/MemberCommandServiceImpl.java
+++ src/main/java/com/shipdoc/domain/Member/service/MemberCommandServiceImpl.java
@@ -7,7 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.shipdoc.domain.Member.converter.MemberConverter;
 import com.shipdoc.domain.Member.entity.Member;
+import com.shipdoc.domain.Member.entity.Role;
+import com.shipdoc.domain.Member.entity.mapping.MemberRole;
+import com.shipdoc.domain.Member.enums.Authority;
 import com.shipdoc.domain.Member.repository.MemberRepository;
+import com.shipdoc.domain.Member.repository.RoleRepository;
 import com.shipdoc.domain.Member.web.dto.MemberRequestDto;
 import com.shipdoc.global.enums.statuscode.ErrorStatus;
 import com.shipdoc.global.exception.GeneralException;
@@ -20,18 +24,33 @@ import lombok.RequiredArgsConstructor;
 public class MemberCommandServiceImpl implements MemberCommandService{
 
 	private final MemberRepository memberRepository;
+	private final RoleRepository roleRepository;
 	private final PasswordEncoder passwordEncoder;
 
+	@Override
 	public Member signup(MemberRequestDto.SignupRequestDto request){
 		// TODO 본인인증 과정 추가
-		if(memberRepository.existsByLoginId(request.getLogin_id())){
+		if(memberRepository.existsByLoginId(request.getLoginId())){
 			throw new GeneralException(ErrorStatus._EXIST_LOGINID);
 		}
 
 		String encodedPassword = passwordEncoder.encode(request.getPassword());
 		Member member = MemberConverter.toMember(request, encodedPassword);
 
+		// 권한 부여
+		Role role = roleRepository.findById(Authority.ROLE_USER.getId()).get();
+
+		MemberRole memberRole = MemberRole.builder().build();
+		role.addMemberRole(memberRole);
+		member.addMemberRole(memberRole);
+
 		return memberRepository.save(member);
+	}
+
+	@Override
+	public void updateRefreshToken(Member member, String reIssuedRefreshToken) {
+		member.changeRefreshToken(reIssuedRefreshToken);
+		memberRepository.saveAndFlush(member);
 	}
 
 }
--- src/main/java/com/shipdoc/domain/Member/service/MemberQueryServiceImpl.java
+++ src/main/java/com/shipdoc/domain/Member/service/MemberQueryServiceImpl.java
@@ -1,12 +1,26 @@
 package com.shipdoc.domain.Member.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.shipdoc.domain.Member.entity.Member;
+import com.shipdoc.domain.Member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class MemberQueryServiceImpl {
+public class MemberQueryServiceImpl implements MemberQueryService{
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public Optional<Member> getMemberWithAuthorities(String loginId) {
+		Member member = memberRepository.findByLoginId(loginId).orElse(null);
+		member.getMemberRoleList().size();
+		return Optional.ofNullable(member);
+	}
 }
