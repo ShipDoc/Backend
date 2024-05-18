72dcb4026bbf3b07ac1db60b933e193a19de3ddb
junhaa <2171326@hansung.ac.kr>
2024-05-18 15:01:33 +0900

    :art: CHORE. SecurityConfig 빈 등록

--- src/main/java/com/shipdoc/global/config/SecurityConfig.java
+++ src/main/java/com/shipdoc/global/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.shipdoc.global.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *  Spring Security config
  */
+@Configuration
 public class SecurityConfig {
 
 	/**
