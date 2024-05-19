e1f2848d2c634c95a2bb0894f19079efa1085953
junhaa <2171326@hansung.ac.kr>
2024-05-20 00:15:24 +0900

    :sparkles: FEAT. 카카오 주변 장소 검색 요청 API 구현

---
+++ src/main/java/com/shipdoc/global/kakao/dto/KakaoResponseDto.java
@@ -0,0 +1,47 @@
+package com.shipdoc.global.kakao.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoResponseDto {
+
+	@JsonProperty("documents")
+	private List<PlaceDetail> placeDetailList;
+
+	@Getter
+	@Setter
+	public static class PlaceDetail implements Comparable<PlaceDetail> {
+		private String distance;
+		@JsonProperty("id")
+		private String KakaoPlaceId;
+
+		private String phone;
+
+		@JsonProperty("place_name")
+		private String placeName;
+
+		@JsonProperty("place_url")
+		private String placeUrl;
+
+		@JsonProperty("road_address_name")
+		private String address;
+
+		@JsonProperty("x")
+		private String longitude;
+
+		@JsonProperty("y")
+		private String latitude;
+
+		@Override
+		public int compareTo(PlaceDetail o) {
+			return Integer.parseInt(this.distance) - Integer.parseInt(o.distance);
+		}
+	}
+
+}
