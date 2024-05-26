4aa08a48f193fe9cf2e36cd2789e1641dba21b40
jungeun123 <147065885+jungeun123@users.noreply.github.com>
2024-05-27 03:09:22 +0900

    :sparkles: Consultation 엔티티 작성

---
+++ src/main/java/com/shipdoc/domain/Member/entity/Consultation.java
@@ -0,0 +1,43 @@
+package com.shipdoc.domain.Member.entity;
+
+import com.shipdoc.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CollectionId;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CONSULTATION")
+@Builder
+public class Consultation extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDateTime reservationDate;
+
+    @Column(name = "visit_count", nullable = false)
+    private Integer visitCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospitalId; // 병원 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patientId; // 환자 ID
+
+    @Column(name = "diagnosis", nullable = false)
+    private String diagnosis; // 진료 결과
+
+    @Column(name = "ai_recommend")
+    private String aiRecommend; // AI 추천 내용
+}
