569fdaee6b7793bdce768235942a170579c69435
junhaa <2171326@hansung.ac.kr>
2024-05-31 01:44:10 +0900

    :bug: FIX. 파일 경로 Spring 관리로 수정

--- src/main/java/com/shipdoc/domain/chatbot/controller/ChatController.java
+++ src/main/java/com/shipdoc/domain/chatbot/controller/ChatController.java
@@ -1,45 +1,49 @@
 package com.shipdoc.domain.chatbot.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.UUID;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.shipdoc.domain.chatbot.config.ChatConfig;
 import com.shipdoc.domain.chatbot.entity.Chat;
 import com.shipdoc.domain.chatbot.service.IChatService;
 import com.shipdoc.global.response.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("api/chatbot")
 public class ChatController {
 
-    @Autowired
-    IChatService chatService;
+	@Autowired
+	IChatService chatService;
 
+	@Value("classpath:test-iwdw-e3b736d87722.json")
+	private Resource jsonFile;
 
-    @CrossOrigin
-    @PostMapping("/msg")
-    @ResponseBody
-    public ApiResponse<String> sendMessage(@RequestBody Chat msg) throws FileNotFoundException, IOException  {
-        File file = new File("src/main/resources/test-iwdw-e3b736d87722.json");
-        String CREDENTIAL_FILE = file.getAbsolutePath();
-        String PROJECT_ID = "test-iwdw";
-        ChatConfig client = new ChatConfig(CREDENTIAL_FILE, PROJECT_ID);
-        String sessionId = UUID.randomUUID().toString();
-        Chat chat = new Chat(msg.getMessageSent(), client.request(sessionId,msg.getMessageSent()));
-        chatService.addChat(chat);
-        return ApiResponse.onSuccess(client.request(sessionId, msg.getMessageSent()));
-    }
+	@CrossOrigin
+	@PostMapping("/msg")
+	@ResponseBody
+	public ApiResponse<String> sendMessage(@RequestBody Chat msg) throws FileNotFoundException, IOException {
+		String jsonFilePath = Paths.get(jsonFile.getURI()).toString();
+		File file = new File(jsonFilePath);
+		String CREDENTIAL_FILE = file.getAbsolutePath();
+		String PROJECT_ID = "test-iwdw";
+		ChatConfig client = new ChatConfig(CREDENTIAL_FILE, PROJECT_ID);
+		String sessionId = UUID.randomUUID().toString();
+		Chat chat = new Chat(msg.getMessageSent(), client.request(sessionId, msg.getMessageSent()));
+		chatService.addChat(chat);
+		return ApiResponse.onSuccess(client.request(sessionId, msg.getMessageSent()));
+	}
 
 }
